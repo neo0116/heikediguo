0af934c8ca8a81f078f3b9c2933dfa85fbc65ed6
neo <neo.qq.com>
2018-01-12 15:15:04 +0800

    hh

--- src/main/java/com/oracle/git/HelloGit.java
+++ src/main/java/com/oracle/git/HelloGit.java
@@ -6,9 +6,4 @@ public class HelloGit {
 		System.out.println("解决冲突");
 		System.out.println("我是分支啊");
 	}
-		public static void main(String[] args) {
-			System.out.println("哈哈哈");
-			
-			System.out.println("fenzhi");
-		}
 }
